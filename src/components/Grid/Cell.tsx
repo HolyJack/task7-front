47f38d043941ead524f48b03e057cd7d2ce0ca85
holyjack <holyjackdev@gmail.com>
2023-12-25 18:34:35 +0400

    fixed cell property

--- src/components/Grid/Cell.tsx
+++ src/components/Grid/Cell.tsx
@@ -2,16 +2,13 @@ export default function Cell({
   bgcolor,
   value,
   textSize,
-  onClick,
 }: {
   bgcolor: string;
   value?: string;
   textSize?: string;
-  onClick: () => void;
 }) {
   return (
     <div
-      onClick={onClick}
       className={`rounded-md text-white text-center font-bold hover:bg-white ${textSize} ${bgcolor}`}
     >
       {value}
